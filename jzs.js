cdcc28721a7284c9e041bd89593d23bfce9217f5
whouare <88071054+whouare@users.noreply.github.com>
2023-09-16 15:32:10 +0800

    Update jzs.js

--- jzs.js
+++ jzs.js
@@ -1,1 +1,12 @@
-
+/**************************************
+@Koohik
+日期:2023年9月16日
+***************************************/ 
+var obj = JSON.parse($response.body);
+ obj.result.buttomStatus = "1";
+ obj.result.objBuyStatus = "1";
+ obj.result.vipButtomStatus = "0";
+ obj.result.chapterObjPrice = "0.0000";
+ obj.result.chapterObjSalePrice = "0.0000";
+ obj.result.chapterObjIsFree = true;
+$done({body : JSON.stringify(obj)});
